fdac0c0c7657dc073795af79b69b42400461a10d
Chris Watrous <clwatrou@us.ibm.com>
2019-11-28 14:51:56 -0600

    helper function to create js class

--- src/main/proto_repl/utils.cljs
+++ src/main/proto_repl/utils.cljs
@@ -6,15 +6,12 @@
 (def ^:private lodash (js/require "lodash"))
 (def ^:private edn-reader (js/require "../lib/proto_repl/edn_reader"))
 
-
 (defn get-bind [obj key]
   (let [value (lodash.get obj (name key))]
     (if (lodash.isFunction value) (.bind value obj) value)))
 
-
 (r/register-default-tag-parser!
   (fn [tag data] {(symbol "#" tag) data}))
-
 
 (defn pretty-edn [edn-string]
   (try
@@ -23,7 +20,6 @@
       (js/console.error "Error formatting repl result:" err)
       edn-string)))
 
-
 (defn edn->display-tree
   "Parses the edn string and returns a displayable tree. A tree is an array
   whose first element is a string of the root of the tree. The rest of the
@@ -31,8 +27,7 @@
   represented by a vector of one element."
   [edn-string]
   (try (.to_display_tree edn-reader edn-string)
-       (catch :default err #js [edn-string])))
-
+       (catch :default err #js[edn-string])))
 
 (defn js->edn
   "Converts a Javascript object to EDN. This is useful when you need to take a
@@ -40,12 +35,11 @@
   [js-data]
   (.js_to_edn edn-reader js-data))
 
-
 (defn edn-saved-values->display-trees [edn-string]
   (try (.saved_values_to_display_tree edn-reader edn-string)
        (catch :default err
          (js/console.log err)
-         #js [])))
+         #js[])))
 
 (defn obj->map [obj]
   (loop [keys (js/Object.keys obj)
@@ -54,3 +48,22 @@
           r (next keys)
           out (assoc! out (keyword f) (aget obj f))]
       (if r (recur r out) (persistent! out)))))
+
+(defn map->obj [m]
+  (let [out #js{}]
+    (doseq [[k v] m]
+      (aset out (if (keyword? k) (name k) k) v))
+    out))
+
+(defn make-js-class [{:keys [name extends constructor static instance]}]
+  (when name (js/Object.defineProperty constructor "name" #js{:value name}))
+  (when extends (js/Object.assign constructor extends))
+  (js/Object.assign constructor (map->obj static))
+  (set! (.-prototype constructor)
+        (js/Object.assign (if extends (js/Object.create (.-prototype extends)) #js{})
+                          #js{:constructor constructor}
+                          (map->obj instance)))
+  constructor)
+
+(defn empty->nil [o]
+  (if (empty? o) nil o))
--- src/main/proto_repl/views/nrepl_connection_view.cljs
+++ src/main/proto_repl/views/nrepl_connection_view.cljs
@@ -2,6 +2,7 @@
   (:require ["atom-space-pen-views" :refer [$ TextEditorView View]
                                     :rename {$ jquery}]
             ["fs" :as fs]
+            [proto-repl.utils :refer [make-js-class empty->nil]]
             [proto-repl.macros :refer-macros [this-fn]]))
 
 (def ^:private default-host-text "localhost")
@@ -14,97 +15,93 @@
                   first)]
     (if path (str (fs/readFileSync path)) "")))
 
-(def ^:private static-methods
-  {:content
-   (this-fn [this]
-     (.div this #js {:class "proto-repl proto-repl-nrepl-connection-dialog"}
-       (fn []
-         (.h3 this #js {:class "icon icon-plug"} "nRepl connection")
-         (.div this #js {:class "block"}
-           (fn []
-             (.label this "Host")
-             (.subview this "hostEditor"
-                       (TextEditorView. #js {:mini true
-                                             :placeholderText default-host-text
-                                             :attributes #js {:tabindex 1}}))))
-         (.div this #js {:class "block"}
-           (fn []
-             (.label this "Port")
-             (.subview this "portEditor"
-                       (TextEditorView. #js {:mini true
-                                             :placeholderText @default-port-text
-                                             :attributes #js {:tabindex 2}})))))))})
 
-(def ^:private instance-methods
-  {:initialize
-   (this-fn [this]
-     (js/console.log (js/Error. "nrepl-connection-view initialize"))
-     (.add js/atom.commands (.-element this)
-           #js {"core:confirm" #(.onConfirm this)
-                "core:cancel" #(.onCancel this)}))
+(def ConnectionViewImpl
+  (make-js-class
+    {:name "ConnectionViewImpl"
+     :extends View
 
-   :show
-   (this-fn [this]
-     (when-not (.-panel this)
-       (set! (.-panel this) (.addModalPanel js/atom.workspace
-                                            #js {:item this :visible false})))
-     (.storeActiveElement this)
-     (.resetEditors this)
-     (-> this .-panel .show)
-     (-> this .-hostEditor .focus)
-     (let [port (get-nrepl-port)]
-       (reset! default-port-text port)
-       (-> this .-portEditor .getModel (.setPlaceholderText port))))
+     :constructor
+     (this-fn [this confirm-callback]
+       (set! (.-confirmCallback this) confirm-callback)
+       (.call View this confirm-callback))
 
-   :storeActiveElement
-   (this-fn [this]
-     (set! (.-previousActiveElement this) (jquery (.-activeElement js/document))))
+     :static
+     {:content
+      (this-fn [this]
+        (.div this #js{:class "proto-repl proto-repl-nrepl-connection-dialog"}
+          (fn []
+            (.h3 this #js{:class "icon icon-plug"} "nRepl connection")
+            (.div this #js{:class "block"}
+              (fn []
+                (.label this "Host")
+                (.subview this "hostEditor"
+                          (TextEditorView. #js{:mini true
+                                               :placeholderText default-host-text
+                                               :attributes #js{:tabindex 1}}))))
+            (.div this #js{:class "block"}
+              (fn []
+                (.label this "Port")
+                (.subview this "portEditor"
+                          (TextEditorView. #js{:mini true
+                                               :placeholderText @default-port-text
+                                               :attributes #js{:tabindex 2}})))))))}
 
-   :resetEditors
-   (this-fn [this]
-     (-> this .-hostEditor (.setText ""))
-     (-> this .-portEditor (.setText "")))
+     :instance
+     {:initialize
+      (this-fn [this]
+        (js/console.log (js/Error. "nrepl-connection-view initialize"))
+        (.add js/atom.commands (.-element this)
+              #js{"core:confirm" #(.onConfirm this)
+                  "core:cancel" #(.onCancel this)}))
 
-   :restoreFocus
-   (this-fn [this] (some-> this .-previousActiveElement .focus))
+      :show
+      (this-fn [this]
+        (when-not (.-panel this)
+          (set! (.-panel this) (.addModalPanel js/atom.workspace
+                                               #js{:item this :visible false})))
+        (.storeActiveElement this)
+        (.resetEditors this)
+        (-> this .-panel .show)
+        (-> this .-hostEditor .focus)
+        (let [port (get-nrepl-port)]
+          (reset! default-port-text port)
+          (-> this .-portEditor .getModel (.setPlaceholderText port))))
 
-   :toggleFocus
-   (this-fn [this]
-     (.focus (if (-> this .-hostEditor .-element .hasFocus)
-               (.-portEditor this) (.-hostEditor this))))
+      :storeActiveElement
+      (this-fn [this]
+        (set! (.-previousActiveElement this) (jquery (.-activeElement js/document))))
 
-   :onCancel
-   (this-fn [this]
-     (some-> this .-panel .hide)
-     (.restoreFocus this))
+      :resetEditors
+      (this-fn [this]
+        (-> this .-hostEditor (.setText ""))
+        (-> this .-portEditor (.setText "")))
 
-   :onConfirm
-   (this-fn [this]
-     (let [host (-> this .-hostEditor .getText)
-           host (if (empty? host) default-host-text host)
-           port (-> this .-portEditor .getText)
-           port (if (empty? port) @default-port-text port)
-           cb (.-confirmCallback this)]
-       (when (fn? cb) (cb {:host host
-                           :port (js/parseInt port)})))
-     (-> this .-panel .hide))})
+      :restoreFocus
+      (this-fn [this] (some-> this .-previousActiveElement .focus))
 
+      :toggleFocus
+      (this-fn [this]
+        (.focus (if (-> this .-hostEditor .-element .hasFocus)
+                  (.-portEditor this) (.-hostEditor this))))
 
-(def ^:private constructor
-  (this-fn [this confirm-callback]
-    (set! (.-confirmCallback this) confirm-callback)
-    (.call View this confirm-callback)))
+      :onCancel
+      (this-fn [this]
+        (some-> this .-panel .hide)
+        (.restoreFocus this))
 
-
-(js/Object.assign constructor View (clj->js static-methods))
-(set! (.-prototype constructor)
-      (js/Object.assign (js/Object.create (.-prototype View))
-                        #js {:constructor constructor}
-                        (clj->js instance-methods)))
+      :onConfirm
+      (this-fn [this]
+        (let [host (-> this .-hostEditor .getText empty->nil (or default-host-text))
+              port (-> this .-portEditor .getText empty->nil (or @default-port-text))
+              cb (.-confirmCallback this)]
+          (when (fn? cb) (cb {:host host
+                              :port (js/parseInt port)})))
+        (-> this .-panel .hide))}}))
 
 
 (defn show-connection-view [callback]
-  (doto (constructor. callback) .show))
+  (doto (ConnectionViewImpl. callback) .show))
 
 
 (defn toggle-focus [connection-view]

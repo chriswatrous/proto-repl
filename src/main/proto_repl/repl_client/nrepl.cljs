55603adaa790b214a077b6328e3c131256ef215d
Chris Watrous <clwatrou@us.ibm.com>
2020-06-29 04:14:39 -0500

    convert the rest of NReplConnection to cljs

--- src/main/proto_repl/repl_client/nrepl.cljs
+++ src/main/proto_repl/repl_client/nrepl.cljs
@@ -1,17 +1,28 @@
 (ns proto-repl.repl-client.nrepl
-  (:require [proto-repl.repl-client.core :refer [ReplClient] :as rc]
+  (:require [clojure.string :as str]
+            [proto-repl.repl-client.core :refer [ReplClient] :as rc]
             [proto-repl.views.repl-view :as rv]))
 
 (def ^:private lodash (js/require "lodash"))
 (def ^:private nrepl (js/require "jg-nrepl-client"))
 (def ^:private ClojureVersion (js/require "../lib/process/clojure-version"))
 (def ^:private EditorUtils (js/require "../lib/editor-utils"))
-(def ^:private NReplConnection (js/require "../lib/process/nrepl-connection"))
 
 (def ^:private default-ns "user")
 
 (defn- notify-error [err]
-  (js/atom.notifications.addError (str "proto-repl") #js {:detail err :dismissable true}))
+  (js/atom.notifications.addError (str "proto-repl") #js{:detail err :dismissable true}))
+
+(defn- contains-namespace-not-found-error? [messages]
+  (->> messages
+       (mapcat #(.-status %))
+       (some #(= % "namespace-not-found"))))
+
+(defn- may-contain-reader-conditional
+  "Returns true if the code might have a reader conditional in it
+  Avoids unnecesary eval'ing for regular code."
+  [code]
+  (str/includes? code "#?"))
 
 (defn- options-to-sessions [{:keys [old]} options callback]
   (cond
@@ -37,7 +48,7 @@
   handling reader conditionals. http://clojure.org/guides/reader_conditionals"
   [{:keys [old]} code]
   (if (and (some-> old .-clojureVersion (.isReaderConditionalSupported))
-           (.codeMayContainReaderConditional old code))
+           (may-contain-reader-conditional code))
     (let [escaped-str (EditorUtils.escapeClojureCodeInString code)]
       (println "wrapped")
       (str "(eval (read-string {:read-cond :allow} " escaped-str "))"))
@@ -60,15 +71,15 @@
                                  {:file (.getPath editor)}))
                 cb-count (atom 0)]
             (.send (.-conn old) (clj->js eval-options)
-              ; FIXME This callback is sometimes called twice for one call to .send so we need to
-              ; guard the retry.
               (fn [err messages]
                 (swap! cb-count inc)
                 (some-> err notify-error)
                 (try
                   ; If the namespace hasn't been defined this will fail. We redefine the Namespace
                   ; and retry.
-                  (if (.namespaceNotFound old messages)
+                  (if (contains-namespace-not-found-error? messages)
+                    ; FIXME This callback is sometimes called twice for one call to .send so we
+                    ; need to guard the retry.
                     (when (and (= @cb-count 1) (not (:retrying options)))
                       (rc/send-command this
                                        command
@@ -119,8 +130,24 @@
           #js{:dismissable true}))
       (callback))))
 
+(defn- start-message-handling
+  "Log any output from the nRepl connection messages"
+  [{:keys [old]} on-message]
+  (.on (-> old .-conn .-messageStream) "messageSequence"
+    (fn [id messages]
+      (when-not (contains-namespace-not-found-error? messages)
+        (doseq [msg messages]
+          ; Set the current ns, but only if the message is in response
+          ; to something sent by the user through the REPL)))))
+          (if (and (.-ns msg) (= (.-session msg) (.-session old)))
+            (aset old "currentNs" (.-ns msg)))
+          (when (or (= (.-session msg) (.-session old))
+                    (and (= (.-session msg) (.-cmdSession old))
+                         (.-out msg)))
+            (on-message msg)))))))
+
 (defn connect-to-nrepl [{:keys [host port on-message on-start on-stop]}]
-  (let [old (NReplConnection.)
+  (let [old #js{}
         host (or (not-empty host) "localhost")
         this (map->NReplClient {:host host :port port :on-stop on-stop :old old})
         conn (.connect nrepl #js {:port port :host host :verbose false})]
@@ -142,7 +169,7 @@
             (determine-clojure-version this
               ; Handle multiple callbacks for this which can happen during REPL
               ; startup with cider-nrepl middleware for some reason.
-              (memoize #(.startMessageHandling old on-message)))
+              (memoize #(start-message-handling this on-message)))
             (.clone conn
               (fn [err messages]
                 (some-> err notify-error)
